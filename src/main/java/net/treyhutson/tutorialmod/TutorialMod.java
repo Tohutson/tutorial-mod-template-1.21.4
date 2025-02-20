744aa312f2d2587022da49c804320bc1d6721f35
Tohutson <tohutson@example.com>
2025-02-20 17:56:37 -0500

    Add custom Item Groups

--- src/main/java/net/treyhutson/tutorialmod/TutorialMod.java
+++ src/main/java/net/treyhutson/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.treyhutson.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.treyhutson.tutorialmod.block.ModBlocks;
+import net.treyhutson.tutorialmod.item.ModItemGroups;
 import net.treyhutson.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
