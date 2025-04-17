a751e790b87eb831004b1a6e122f2330ae82320f
Tohutson <tohutson@example.com>
2025-04-16 21:28:29 -0400

    Update block registering

--- src/main/java/net/treyhutson/tutorialmod/item/ModItems.java
+++ src/main/java/net/treyhutson/tutorialmod/item/ModItems.java
@@ -1,10 +1,7 @@
 package net.treyhutson.tutorialmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -14,16 +11,23 @@ import net.treyhutson.tutorialmod.TutorialMod;
 import net.treyhutson.tutorialmod.entity.ModEntities;
 import net.treyhutson.tutorialmod.item.custom.ChiselItem;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, "pink_garnet")))));
-    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, "raw_pink_garnet")))));
+    public static final Item PINK_GARNET = registerItem("pink_garnet", Item::new, new Item.Settings());
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", Item::new, new Item.Settings());
 
-    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, "chisel"))).maxDamage(32)));
+    public static final Item CHISEL = registerItem("chisel", ChiselItem::new, new Item.Settings().maxDamage(32));
 
-    public static final Item MANTIS_SPAWN_EGG = registerItem("mantis_spawn_egg", new SpawnEggItem(ModEntities.MANTIS, new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, "mantis_spawn_egg")))));
+    public static final Item MANTIS_SPAWN_EGG = registerItem(
+            "mantis_spawn_egg",
+            settings -> new SpawnEggItem(ModEntities.MANTIS, settings),
+            new Item.Settings()
+    );
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, path));
+        return Items.register(registryKey, factory, settings);
     }
 
     public static void registerModItems() {
